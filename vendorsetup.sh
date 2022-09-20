5a1971121b1f945d6168212d02338827c60582c6
imannig <zahranansaria@gmail.com>
2022-09-20 21:00:11 +0700

    chime: Add vendorsetup.sh
    Signed-off-by: imannig <zahranansaria@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,11 @@
+# ROM source patches
+
+color="\033[0;32m"
+end="\033[0m"
+
+echo -e "${color}Applying patches${end}"
+sleep 1
+
+# Clone treee
+git clone https://github.com/ProjectElixir-Devices/android_kernel_xiaomi_chime --depth=1 -b twelve kernel/xiaomi/chime
+git clone https://github.com/ProjectElixir-Devices/android_vendor_xiaomi_chime -b thirteen vendor/xiaomi/chime
