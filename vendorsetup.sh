60a02c2867d3ac84faa206539ab2f3be11fb9931
Zahran Ansaria <zahranansaria@gmail.com>
2022-10-16 21:45:05 +0700

    chime: vendorsetup: Change kernel branch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,6 +10,6 @@ sleep 1
 rm -rf device/qcom/sepolicy-legacy-um
 
 # Clone treee
-git clone https://github.com/ProjectElixir-Devices/android_kernel_xiaomi_chime --depth=1 -b twelve kernel/xiaomi/chime
+git clone https://github.com/ProjectElixir-Devices/android_kernel_xiaomi_chime --depth=1 -b tiramisu kernel/xiaomi/chime
 git clone https://gitlab.com/RayapKun/vendor_xiaomi_chime -b thirteen vendor/xiaomi/chime
 git clone https://github.com/Evolution-X/device_qcom_sepolicy_vndr-legacy-um device/qcom/sepolicy_vndr-legacy-um -b tiramisu
