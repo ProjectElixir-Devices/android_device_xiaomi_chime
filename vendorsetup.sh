6310e5de8d08da1527eae3969b799a402f4be92f
imannig <zahranansaria@gmail.com>
2022-11-22 22:46:56 +0700

    chime: Add vendorsetup.sh and remove.txt
    Signed-off-by: imannig <zahranansaria@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,12 @@
+# ROM source patches
+
+color="\033[0;32m"
+end="\033[0m"
+
+echo -e "${color}Applying patches${end}"
+sleep 1
+
+
+# Clone treee
+git clone https://github.com/ProjectElixir-Devices/android_kernel_xiaomi_chime --depth=1 -b twelve kernel/xiaomi/chime
+git clone https://gitlab.com/cosm1f/vendor_xiaomi_chime -b tokyo vendor/xiaomi/chime
