71f6f320011bb3aa45be46f3701df5cccc2d53f3
imannig <zahranansaria@gmail.com>
2022-11-30 18:01:47 +0700

    chime: Clone compat and hardware hals
    Signed-off-by: imannig <zahranansaria@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,3 +10,8 @@ sleep 1
 # Clone treee
 git clone https://github.com/ProjectElixir-Devices/android_kernel_xiaomi_chime --depth=1 -b twelve kernel/xiaomi/chime
 git clone https://gitlab.com/cosm1f/vendor_xiaomi_chime -b tokyo vendor/xiaomi/chime
+
+# Hardware
+rm -rf hardware/xiaomi
+git clone https://github.com/csmqfest/android_hardware_xiaomi hardware/xiaomi
+git clone https://github.com/PixelExperience/hardware_custom_compat hardware/custom/compat
