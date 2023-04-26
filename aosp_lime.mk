4556a940d3a809999c5a91b3023f01257d4dcf1d
imannig <zahranansaria@gmail.com>
2023-04-26 17:32:20 +0700

    chime: Adapt to Elixir
    Signed-off-by: imannig <zahranansaria@gmail.com>

--- aosp_lime.mk
+++ aosp_lime.mk
@@ -7,7 +7,7 @@
 # Inherit from lime device
 $(call inherit-product, device/xiaomi/chime/device-lime.mk)
 
-# Inherit some common PixelExperience stuff.
+# Inherit some common ProjectElixir stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_USES_AOSP_RECOVERY := true
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -21,3 +21,12 @@ PRODUCT_MODEL := Redmi 9T
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Elixir Stuff
+ELIXIR_MAINTAINER := imannig
+ELIXIR_BUILD_TYPE := OFFICIAL
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_ENABLE_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
