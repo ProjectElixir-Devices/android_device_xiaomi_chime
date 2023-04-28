70b45af6f43d4ab6abb0309f08cfbd1e92d37fa2
imannig <zahranansaria@gmail.com>
2023-04-28 16:18:52 +0000

    chime: Add vendorsetup.sh and Remove.txt

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+# ROM source patches
+
+color="\033[0;32m"
+end="\033[0m"
+
+echo -e "${color}Applying patches${end}"
+sleep 1
+
+git clone https://github.com/imannig/kernel_xiaomi_chime --depth=1 kernel/xiaomi/chime
+git clone https://gitlab.com/imannig/dump_chime vendor/xiaomi/chime
